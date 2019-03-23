719bbbdb0d0a0b6f073cf690a7090ea364952262
Ashwin <ashwin.k.sri@gmail.com>
2019-03-23 20:58:03 +0530

    Add files to deploy to Heroku

--- DroneWebApp/settings.py
+++ DroneWebApp/settings.py
@@ -125,9 +125,9 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
